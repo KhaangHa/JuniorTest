804710e37a0b2fee07b1e3b8824fc99962ed7469
Ha Duy Khang <haduykhang109@gmail.com>
2019-09-18 10:11:57 +0700

    [Chapter 2] Add size to clock, add config for system

--- Block/Header.php
+++ Block/Header.php
@@ -1,22 +1,44 @@
 <?php
 namespace Magenest\Junior\Block;
-use \Magento\Framework\App\Config\ScopeConfigInterface;
 use \Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Header
+ * @package Magenest\Junior\Block
+ */
 class Header extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @return mixed
+     */
     public function getTextColor()
     {
         $textColor = $this->_scopeConfig->getValue('magenest_clock/general/text_color', ScopeInterface::SCOPE_WEBSITE);
         return $textColor;
     }
+
+    /**
+     * @return mixed
+     */
     public function getBackgroundColor()
     {
         $color = $this->_scopeConfig->getValue('magenest_clock/general/clock_color', ScopeInterface::SCOPE_WEBSITE);
         return $color;
     }
+
+    /**
+     * @return mixed
+     */
     public function getTitle()
     {
         return $this->_scopeConfig->getValue('magenest_clock/general/clock_title', ScopeInterface::SCOPE_WEBSITE);
     }
 
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->_scopeConfig->getValue('magenest_clock/general/clock_size', ScopeInterface::SCOPE_WEBSITE);
+    }
 }
